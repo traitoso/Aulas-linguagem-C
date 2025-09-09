8e01a9f1667da56b0dbaf61d04793ec1341a9d96
Alisson de oliveira da Silva <traitoso2005@gmail.com>
2025-09-09 20:33:15 -0300

    Update Ano_bissexto.c

--- Ano_bissexto.c
+++ Ano_bissexto.c
@@ -3,14 +3,14 @@
 int main()
 {
    int ano;
-  printf("digite o ano);
+  printf("digite o ano\n");
   scanf("%d", &ano );
   if(ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0){
-    printf("o ano %f é bissexto", ano);
+    printf("o ano %d é bissexto\n", ano);
   }
   else{
-    printf("o ano %f não é bissexto", ano);
+    printf("o ano %d não é bissexto\n", ano);
   }
-    printf("finalizando o programa...");
+    printf("finalizando o programa...\n");
     return 0;
 }
